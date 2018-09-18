5399f55c96a2359ed7f983e04ba76145c2f55f1d
Anusha Konatala <anu.konatala@gmail.com>
2018-09-18 05:38:51 +0000

    automatically backed up by learn

--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -11,7 +11,12 @@ const imgMapper = {0: zero, 1: one, 2: two, 3: three, 4: four, 5: five}
 export default class CardBack extends Component {
 
   generateRatingElement = () => {
-    // implement meeeee! See the readme for instructions
+    let checkValue = this.props.IMDBRating
+    if(checkValue !== null){
+      return <img src={imgMapper[this.props.IMDBRating]} alt="" />
+    } else {
+      return <h4>No Rating Found</h4>
+    }
   }
 
   render() {
