13cc07118bec73663c3c24524adba3c18a9bcda4
Anusha Konatala <anu.konatala@gmail.com>
2018-09-18 05:35:38 +0000

    automatically backed up by learn

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -32,8 +32,8 @@ export default class MovieCard extends Component {
   render() {
     return (
       <div className="movie-card">
-        <CardFront />
-        <CardBack />
+         <CardFront poster={ posterMap[this.props.poster] } />
+        <CardBack genres={ this.props.genres } IMDBRating={ this.props.IMDBRating } title={ this.props.title }/>
       </div>
     )
   }
